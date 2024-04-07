be01bbdd536fd3db8566b123b3fcbbe2aed3117e
Louie Lu <jqilu@ucdavis.edu>
2024-04-06 23:12:48 -0400

    Extracted out the read_tsv_to_df() from jupyter notebook to be its own function in the module

--- dataset_cleanup.py
+++ dataset_cleanup.py
@@ -3,7 +3,24 @@ import pandas as pd
 from sklearn.feature_selection import \
     VarianceThreshold  # For checking descriptors with low variance
 
+def read_tsv_to_df(tsv_file_directory):
+    """
+    Convert the .tsv file to pd df
 
+    Args:
+        tsv_file_directory (str): directory for the .tsv file
+
+    Returns:
+        df (pd df): df that contains the data from the .csv file
+
+    """
+    df = pd.read_csv(
+        tsv_file_directory,
+        sep='\t'  # .tsv files are similar to .csv files but instead of using
+        # comma, .tsv files use tabs as separator
+    )
+
+    return df
 
 def filter_low_variance_worker(intput_df, threshold_level):
     """
